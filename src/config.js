f342f506b23acea72643c1f0bb5d9b09536ebfba
ecoulson <ivansonofcoul@gmail.com>
2017-01-26 22:13:17 -0800

    memes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   'secret': 'DankMemesCantMeltSteelBeams',
-  'db': 'mongodb://localhost/blog',
+  'db': 'mongodb://heroku_0cdstvjc:3g7472olr7fjap1eccob7kqhvg@ds133249.mlab.com:33249/heroku_0cdstvjc',
   'googleID':'281949539504-lnt4nv87g08s86ecmgfs465m3vku2ok7.apps.googleusercontent.com',
   'googleSecret':'j7Ka_H0Lk-eJQZ5XHetWXz5j',
   'facebookID':'932143540254978',
